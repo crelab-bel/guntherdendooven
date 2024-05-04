cef931cb671694a18ae0267fe357e99d91687b17
crelab-bel <168651978+crelab-bel@users.noreply.github.com>
2024-05-04 17:08:48 +0200

    Add Base64 encoded file

--- app/content.js
+++ app/content.js
@@ -1,4 +1,4 @@
 export const mainHero = {
-    h1: 'Plaats een SEO vriendelijke Hero tekst hier',
-    paragraaf: 'Plaats een SEO vriendelijke text die de H1 text ondersteunt hier',
+    h1: 'Ontdek de Beste Foodtruck Verhuur voor Elk Evenement in België',
+    paragraaf: 'Van smakelijke festivals tot onvergetelijke privéfeesten, onze veelzijdige foodtrucks brengen culinaire hoogstandjes direct naar jouw locatie. Ontdek nu ons uitgebreide aanbod en boek de perfecte foodtruck voor jouw volgende evenement!',
 };
